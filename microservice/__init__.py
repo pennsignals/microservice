633037a9496a2dfecd4e73e9b657330b81289bab
Jason Lubken <jlubken@users.noreply.github.com>
2018-10-17 13:28:50 -0400

    Update ignores and setup

--- microservice/__init__.py
+++ microservice/__init__.py
@@ -1,9 +1,28 @@
 """Microservice."""
 
+from logging import (
+    basicConfig,
+    getLogger,
+    INFO,
+)
+from os import stdout
+
 from .core import Microservice as BaseMicroservice
+
+from pkg_resources import (
+    DistributionNotFound,
+    get_distribution,
+)
+
+basicConfig(
+    level=INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=stdout)
+logger = getLogger(__name__)
 
 
 class Microservice(BaseMicroservice):
+    """Microservice."""
 
     CONFIGURATION = {
         ('MICROSERVICE_CONFIGURATION', '--cfg'): {
@@ -22,9 +41,10 @@ class Microservice(BaseMicroservice):
 
     @classmethod
     def main(cls):
+        """Run job inside scheduler from configuration."""
         raise NotImplementedError()
 
     @classmethod
     def job(cls):
+        """Run job from configuration."""
         raise NotImplementedError()
-
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 """Setup ventilator model."""
 
-from setuptools import find_packages, setup
+from datetime import date
+from os.path import (
+    exists,
+    join,
+)
+from setuptools import (
+    find_packages,
+    setup,
+)
 
-TESTS_REQUIRE = [
+TESTS_REQUIRE = (
     'flake8-bugbear',
     'flake8-commas',
     'flake8-comprehensions',
@@ -17,38 +25,50 @@ TESTS_REQUIRE = [
     'pytest-flake8',
     'pytest-mock',
     'pytest-pylint',
-]
+)
+
+
+def get_long_description(file_name='readme.md'):
+    """Get long description."""
+    with open(join(file_name), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
     name='microservice',
     author='Penn Medicine Predictive Healthcare',
-    version='0.1',
-    classifiers=[
+    classifiers=(
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
-    ],
+    ),
     entry_points={
-        'console_scripts': [
+        'console_scripts': (
             'microservice = microservice:Microservice.main',
-            'microservice.job = microservice:Microservice.job'
-        ],
+            'microservice.job = microservice:Microservice.job',
+        ),
     },
     extras_require={
         'tests': TESTS_REQUIRE,
     },
     include_package_data=True,
-    install_requires=[
+    install_requires=(
         'curio',
         'numpy',
         'pandas',
         'pymongo',
         'pymssql',
         'pyyaml',
-    ],
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    ),
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=('tests', 'tests.*')),
     python_requires='>=3.6',
-    setup_requires=['pytest-runner'],
+    setup_requires=('pytest-runner', 'setuptools_scm'),
     tests_require=TESTS_REQUIRE,
     url='https://github.com/pennsignals/microservice',
-    # use_scm_version=True,
+    use_scm_version=dict(
+        local_scheme='dirty-tag',
+        version_scheme='guess-next-dev',
+        write_to='microservice/__version__.py',
+    ),
 )
